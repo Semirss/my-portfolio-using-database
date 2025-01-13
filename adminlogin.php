9f461fe4a0234d08fd50da9630441d33c307c55b
Semir <149691609+Semirss@users.noreply.github.com>
2025-01-13 10:56:05 -0900

    Update adminlogin.php

--- adminlogin.php
+++ adminlogin.php
@@ -1,18 +1,6 @@
 <?php
+include "dbcon.php";
 session_start();
-
-$servername = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$dbname = "portfolio"; 
-
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 // Check if the form is submitted
 if (isset($_POST['login'])) {
